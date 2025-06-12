6fe9fa78407ac9aedcfb5852ddd83375dc35315e
jsbarbuto <josephbarbuto03@gmail.com>
2025-06-12 17:37:41 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 const weatherData = {'2025-06-08': ['Rome', '15'], '2025-06-09': ['Brisbane', '15'], '2025-06-10': ['Mendoza', '15'], '2025-06-11': ['Rome', '15'], '2025-06-12': ['Rome', '15'], '2025-06-13': ['Rome', '15'], '2025-06-14': ['Vienna', '15'], '2025-06-15': ['Vienna', '15'], '2025-06-16': ['Brisbane', '15'], '2025-06-17': ['Vienna', '15'], '2025-06-18': ['Vienna', '15'], '2025-06-19': ['Buenos Aires', '15'], '2025-06-20': ['Buenos Aires', '15'], '2025-06-21': ['Buenos Aires', '15'], '2025-06-22': ['C�rdoba', '15'], '2025-06-23': ['Toronto', '15'], '2025-06-24': ['Toronto', '15'], '2025-06-25': ['Athens', '15'], '2025-06-26': ['Valletta', '15']}
 
-console.log(weatherData['2025-06-08']);
-
 console.log(`example dates 20250609, 20250615`);
-//YYYYMMDD
-//20250609
 let dateFrom
 let dateBack
 //dataset format 2025-06-10
-
 
 document.getElementById("dateFromButton").onclick = function(){ 
     dateFrom = document.getElementById("dateFrom").value;
     dateFrom = formatDate(dateFrom);
 
     console.log("date From: " + dateFrom); 
-
 }
 
 document.getElementById("dateBackButton").onclick = function(){
     dateBack = document.getElementById("dateBack").value;
-    console.log("date To: " + dateBack); 
     dateBack = formatDate(dateBack);
 
-    document.getElementById("myH2").textContent = `${dateFrom} - ${dateBack} ....`;
+    console.log("date To: " + dateBack); 
 }
 
 document.getElementById("generateButton").onclick = function(){
@@ -32,15 +25,16 @@ document.getElementById("generateButton").onclick = function(){
     console.log(`dates: ${dates}`);
 
     let winner = bestLocation(dates, weatherData);
-    console.log(`best country: ${winner}`);
+    console.log(`best location: ${winner}`);
 
-    //write best location
-    document.getElementById("myH2").textContent = `${winner}!!`;
+    document.getElementById("myH2").textContent = 
+    `For dates ${dateFrom} to ${dateBack}:`;
+
+    document.getElementById("myH2").textContent = `the nicest weather is in ${winner}!!`;
 
     console.log(`recommended city is ${winner}`);
 }
 
-//window.alert('example');
 
 function formatDate(date){
     stringBuilder = "";
@@ -57,29 +51,28 @@ function getDateRange(startDateStr, endDateStr) {
   const startDate = new Date(startDateStr);
   const endDate = new Date(endDateStr);
 
-  //prevent DST issues
+  // prevent DST issues
   startDate.setHours(0, 0, 0, 0);
   endDate.setHours(0, 0, 0, 0);
 
   while (startDate <= endDate) {
-    //get string (YYYY-MM-DD T 00:00:00.000Z)
+    // get string (YYYY-MM-DD T 00:00:00.000Z)
     const formatted = startDate.toISOString().split('T')[0];
     dates.push(formatted);
 
-    //next day
+    // next day
     startDate.setDate(startDate.getDate() + 1);
   }
 
   return dates;
 }
 
-//data -> date: [city, score]
 function bestLocation(dates){
-    //dict -> city : score
+    //dict format city : score
     let dict = {};
 
     for (let d of dates){
-        //cityScore -> tokyo, 10
+        //cityScore format tokyo, 10
         let cityScore = weatherData[d];
         let city = cityScore[0];
         let dataScore = cityScore[1];
